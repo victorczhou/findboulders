3fb2835b74048f06bc8399f023252549df02cf69
victorczhou <victorczhou@gmail.com>
2020-07-05 23:16:03 -0700

    added training scraper

--- training_scraper.py
+++ training_scraper.py
@@ -11,6 +11,21 @@ password = "boulders"
 database = "boulder_training"
 trainingfile = "bishop_3star.csv"
 
+styles = ["dyno", "crack", "traverse", "steep", "technical", "mantle", "face"]
+angles = ["slab", "overhanging", "vertical"]
+
+def parse_grade(gradestring):
+	if  "V-easy" in gradestring:
+		return 0
+	else:
+		holdmatch = re.search("V[0-9]+", gradestring)
+		if holdmatch is not None:
+			holdv = holdmatch.group()
+			return holdv[1:]
+		else:
+			print("Error: invalid grade. -1 inserted.")
+			return -1
+
 conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 cur = conn.cursor()
 
@@ -24,27 +39,29 @@ with open(file_ext, 'r', encoding='utf-8') as csvfile:
 
 		url = row[2]
 		grade = parse_grade(row[6])
-		risk = parse_risk(row[6])
-		holdstyle = "face"
-		holdangle = "overhanging"
-		holdlocation = "California"	# parse later not sorting by area
+		desc = ""
+		holdangle = "0"
+		holdstyle = "0"
 
-		if "Traverse" in row[0]:
-			holdstyle = "traverse"
+		page = requests.get(url) 	# MP has static HTML
+		soup = BeautifulSoup(page.content, 'html.parser')
+		desc_element = soup.find('div', class_='fr-view') 	# MP has two <div class="fr-view"> for desc and location, respectively
+		if desc_element is None:
+			print("Error: cannot find description for climb ", row[0], ".")
+			continue
 		else:
-			page = requests.get(url) 	# MP has static HTML
-			soup = BeautifulSoup(page.content, 'html.parser')
-			desc_element = soup.find('div', class_='fr-view') 	# MP has two <div class="fr-view"> for desc and location, respectively
-			if desc_element is None:
-				print("Error: cannot find description for climb ", row[0], ".")
-				continue
-			else:
-				holdstyle = parse_style(desc_element.text.strip())
-				holdangle = parse_angle(desc_element.text.strip())
+			desc = desc_element.text.strip()
+			print(row[0], " ", grade)
+			print("STYLE: dyno, crack, traverse, steep, technical, mantle, face")
+			while holdstyle not in styles:
+				holdstyle = input()
+			print("ANGLE: slab, overhanging, vertical")
+			while holdangle not in angles:
+				holdangle = input()
 			
-		cur.execute("INSERT INTO ca_boulders (name, grade, rating, climb_style, climb_angle, lat, lon, url, location, climb_risk) \
-			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", \
-			(row[0], grade, row[3], holdstyle, holdangle, row[9], row[10], url, holdlocation, risk))
+		cur.execute("INSERT INTO boulder_training (name, grade, climb_angle, climb_style, description, url) \
+			VALUES (%s, %s, %s, %s, %s, %s);", \
+			(row[0], grade, holdangle, holdstyle, desc, url))
 		conn.commit()
 
 

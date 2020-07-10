df2e64b102438d9035ef206d0f56e236240c20fa
victorczhou <victorczhou@gmail.com>
2020-07-10 00:22:45 -0700

    added algs

--- classifier.py
+++ classifier.py
@@ -1,9 +1,17 @@
 import pandas as pd 
 import re
 import nltk
+#nltk.download("stopwords") 			# uncomment if FIRST TIME RUNNING
 from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
 
 filename = "boulder_training.csv"
+styles = ["dyno", "crack", "traverse", "steep", "technical", "mantle", "face"]
 STOPWORDS = set(stopwords.words("english"))
 
 def clean_text(text):
@@ -29,9 +37,55 @@ def clean_text(text):
     text = text.translate(translate_map)
 
     #remove stopwords
-    text = " ".join(word for word in text.split() if word is not in STOPWORDS)
+    text = " ".join(word for word in text.split() if word not in STOPWORDS)
 
     return text
 
+def naive_bayes(x_train, x_test, y_train, y_test):
+	from sklearn.naive_bayes import MultinomialNB
+
+	nb = Pipeline([("vect", CountVectorizer()),
+	               ("tfidf", TfidfTransformer()),
+	               ("clf", MultinomialNB()),
+	              ])
+	nb.fit(x_train, y_train)
+	y_pred = nb.predict(x_test)
+
+	print("NB Accuracy %s" % accuracy_score(y_pred, y_test))
+	print(classification_report(y_test, y_pred, target_names=styles))
+
+def linear_svm(x_train, x_test, y_train, y_test):
+	from sklearn.linear_model import SGDClassifier
+
+	sgd = Pipeline([("vect", CountVectorizer()),
+	                ("tfidf", TfidfTransformer()),
+	                ("clf", SGDClassifier(alpha=.001, max_iter=10, random_state=1)), # hinge loss, l2 reg
+	               ])
+	sgd.fit(x_train, y_train)
+	y_pred = sgd.predict(x_test)
+
+	print("SVM Accuracy %s" % accuracy_score(y_pred, y_test))
+	print(classification_report(y_test, y_pred, target_names=styles))
+
+def log_reg(x_train, x_test, y_train, y_test):
+	from sklearn.linear_model import LogisticRegression
+
+	logreg = Pipeline([("vect", CountVectorizer()),
+	                ("tfidf", TfidfTransformer()),
+	                ("clf", LogisticRegression(n_jobs=1, C=1e5)),
+	               ])
+	logreg.fit(x_train, y_train)
+	y_pred = logreg.predict(x_test)
+
+	print("LogReg Accuracy %s" % accuracy_score(y_pred, y_test))
+	print(classification_report(y_test, y_pred, target_names=styles))
+
 df = pd.read_csv(filename)
 df["description"] = df["description"].apply(clean_text)
+x = df.description		# i think
+y = df.climb_style
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=10)
+
+naive_bayes(x_train, x_test, y_train, y_test)
+linear_svm(x_train, x_test, y_train, y_test)
+log_reg(x_train, x_test, y_train, y_test)

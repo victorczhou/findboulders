fa58b886c1ec5d6cdcf166677b15cb3673207e69
victorczhou <victorczhou@gmail.com>
2020-07-09 21:26:58 -0700

    added stopwords

--- classifier.py
+++ classifier.py
@@ -1,7 +1,10 @@
 import pandas as pd 
 import re
+import nltk
+from nltk.corpus import stopwords
 
 filename = "boulder_training.csv"
+STOPWORDS = set(stopwords.words("english"))
 
 def clean_text(text):
     # remove HTML tags
@@ -9,12 +12,12 @@ def clean_text(text):
     
     # remove the characters [\], ['] and ["] and 0x91-0x94
     text = re.sub(r"\\", "", text)    
-    text = re.sub(r"\'", "", text)    
-    text = re.sub(r"\"", "", text)
-    text = re.sub(r"", "", text) 
-    text = re.sub(r"", "", text) 
-    text = re.sub(r"", "", text)
-    text = re.sub(r"", "", text)
+    text = re.sub(r"\'", " ", text)    			# filters all quotes into spaces -> stopwords remove s
+    text = re.sub(r"\"", " ", text)
+    text = re.sub(r"", " ", text) 
+    text = re.sub(r"", " ", text) 
+    text = re.sub(r"", " ", text)
+    text = re.sub(r"", " ", text)
     
     # convert text to lowercase
     text = text.strip().lower()
@@ -25,9 +28,10 @@ def clean_text(text):
     translate_map = str.maketrans(translate_dict)
     text = text.translate(translate_map)
 
+    #remove stopwords
+    text = " ".join(word for word in text.split() if word is not in STOPWORDS)
+
     return text
 
 df = pd.read_csv(filename)
 df["description"] = df["description"].apply(clean_text)
-
-print(df)

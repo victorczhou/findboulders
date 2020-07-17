4f31682a7dc732579a9d37326655bedef620aa1a
victorczhou <victorczhou@gmail.com>
2020-07-17 00:59:06 -0700

    tuned

--- classifier.py
+++ classifier.py
@@ -100,7 +100,7 @@ def neural_net(x_train, x_test, y_train, y_test):
 		for style in styles:
 			hold.append(ston[style])
 		return hold
-'''
+	'''
 	def num_to_style(nums):
 		ntos = {
 			0 : "dyno",
@@ -115,7 +115,7 @@ def neural_net(x_train, x_test, y_train, y_test):
 		for num in nums:
 			hold.append(ntos[num])
 		return hold
-'''
+	'''
 	vectorizer = CountVectorizer()
 	vectorizer.fit(x_train)
 	xvec_train = vectorizer.transform(x_train)
@@ -125,14 +125,13 @@ def neural_net(x_train, x_test, y_train, y_test):
 	y_train_nums = np.array(style_to_num(y_train))
 
 	model = Sequential()
-	model.add(hub_layer)
 	model.add(layers.Dense(32, activation='relu', input_dim=xvec_train.shape[1]))			# tune node hyperparameter - not much diff
 	model.add(layers.Dense(7)) 																# 7 classes
 
 	model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 	# mutually exclusive
               metrics=['accuracy'])
 	#model.summary()
-	model.fit(xvec_train, y_train_nums, validation_data=(xvec_test, y_test_nums), epochs=20, batch_size=10, verbose=1)
+	model.fit(xvec_train, y_train_nums, validation_data=(xvec_test, y_test_nums), epochs=10, batch_size=10, verbose=1)
 	result = model.evaluate(xvec_test, y_test_nums, verbose=2)
 	print("Keras Loss, Accuracy: ", result)
 
@@ -141,22 +140,22 @@ def neural_net(x_train, x_test, y_train, y_test):
                            dtype=tf.string, trainable=True)
 	model2 = Sequential()
 	model2.add(hub_layer)
-	model2.add(layers.Dense(32, activation='relu', input_dim=xvec_train.shape[1]))
+	model2.add(layers.Dense(32, activation='relu'))
 	model2.add(layers.Dense(7))
 	model2.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-	model2.fit(xvec_train, y_train_nums, validation_data=(xvec_test, y_test_nums), epochs=20, batch_size=10, verbose=1)
-	result = model2.evaluate(xvec_test, y_test_nums, verbose=2)
+	model2.fit(x_train, y_train_nums, validation_data=(x_test, y_test_nums), epochs=50, batch_size=10, verbose=1)
+	result = model2.evaluate(x_test, y_test_nums, verbose=2)
 	print("Keras Loss, Accuracy: (pretrained)", result)
 
 
 df = pd.read_csv(filename)
 df["description"] = df["description"].apply(clean_text)
-df.info()
+#df.info()
 x = df.description		# i think
 y = df.climb_style
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=10)
 
-#naive_bayes(x_train, x_test, y_train, y_test)
-#linear_svm(x_train, x_test, y_train, y_test)
-#log_reg(x_train, x_test, y_train, y_test)
+naive_bayes(x_train, x_test, y_train, y_test)
+linear_svm(x_train, x_test, y_train, y_test)
+log_reg(x_train, x_test, y_train, y_test)
 neural_net(x_train, x_test, y_train, y_test)
